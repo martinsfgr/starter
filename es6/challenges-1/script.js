b7db0e7b2e2dbacc3e4025586f9de2936cba25a6
Mauricio <mauriciofagner2010@hotmail.com>
2020-09-13 12:27:16 -0300

    Finalizar a primeira tarefa

---
+++ es6/challenges-1/script.js
@@ -0,0 +1,23 @@
+class User {
+  constructor() {
+    this.email = String;
+    this.password = String;
+    this.admin = false;
+  }
+
+  isAdmin() {
+    return this.admin;
+  }
+}
+
+class Admin extends User {
+  constructor() {
+    super();
+    this.admin = true;
+  }
+}
+
+const user = new User('email@email.com', 'senha123');
+const admin = new Admin('email@email.com', 'senha123');
+
+console.log(user.isAdmin(), admin.isAdmin());
