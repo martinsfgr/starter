9a8e324987ab9d2cb2042057ac2e521bdd77108b
Mauricio <mauriciofagner2010@hotmail.com>
2020-09-15 23:00:35 -0300

    Finalizar desafio de template literals e object short syntax

---
+++ es6/challenges-1/sixth-challenge.js
@@ -0,0 +1,11 @@
+const nomeUsuario1 = 'Mauricio';
+const idadeUsuario1 = 19;
+
+console.log(`O usuário ${nomeUsuario1} possui ${idadeUsuario1} anos`);
+
+const usuarioOss = {
+  nomeUsuario1,
+  idadeUsuario1
+}
+
+console.log(usuarioOss);
