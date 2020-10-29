93d495c3d5c14e99ce494a54c6b40c0481166bbe
Mauricio <mauriciofagner2010@hotmail.com>
2020-10-29 13:02:29 -0300

    Fazer tratamento de erro e mostrar o loading (app es6+)

--- es6/app-es6+/src/main.js
+++ es6/app-es6+/src/main.js
@@ -15,29 +15,50 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
+  setLoading(loading = true) {
+    if (loading) {
+      let loadingEl = document.createElement('span');
+      loadingEl.appendChild(document.createTextNode('Carregando'));
+      loadingEl.setAttribute('id', 'loading');
+
+      this.formEl.appendChild(loadingEl);
+    } else {
+      document.getElementById('loading').remove();
+    }
+  }
+
   async addRepository(event) {
     event.preventDefault();
 
     const repoInput = this.inputEl.value;
 
     if (repoInput.length === 0) {
-      alert('Procure por algum repositório.')
+      alert('Procure por algum repositório.');
+      return;
     }
 
-    const response = await api.get(`/repos/${repoInput}`);
+    this.setLoading();
 
-    const { name, description, html_url, owner: { avatar_url } } = response.data;
+    try {
+      const response = await api.get(`/repos/${repoInput}`);
+      
+      const { name, description, html_url, owner: { avatar_url } } = response.data;
+    
+      this.repositories.push({
+        name: name,
+        description: description,
+        avatar_url: avatar_url,
+        html_url: html_url
+      });
+    
+      this.inputEl.value = '';
+    
+      this.render();
+    } catch (err) {
+      alert('O repositório não existe');
+    }
 
-    this.repositories.push({
-      name: name,
-      description: description,
-      avatar_url: avatar_url,
-      html_url: html_url
-    });
-
-    this.inputEl.value = '';
-
-    this.render();
+    this.setLoading(false);
   }
 
   render() {
