a2b2c00b3a9317fc9b2bbed782a6672605e8a849
Mauricio <mauriciofagner2010@hotmail.com>
2020-10-29 12:40:55 -0300

    Adicionar repositórios e renderizar em tela (app es6+)

---
+++ es6/app-es6+/src/main.js
@@ -0,0 +1,57 @@
+class App {
+  constructor() {
+    this.repositories = [];
+    
+    this.formEl = document.getElementById('repo-form');
+    this.listEl = document.getElementById('repo-list');
+    
+    this.registerHandlers();
+  }
+
+  registerHandlers() {
+    this.formEl.onsubmit = event => this.addRepository(event);
+  }
+
+  addRepository(event) {
+    event.preventDefault();
+
+    this.repositories.push({
+      name: 'Mauricio',
+      description: 'Descrição',
+      avatar_url: 'https://avatars1.githubusercontent.com/u/52453558?s=460&u=cb6dd08efea0250e5067e084e593196ef46869be&v=4',
+      html_url: 'www.google.com',
+    });
+
+    this.render();
+  }
+
+  render() {
+    this.listEl.innerHTML = '';
+
+    this.repositories.forEach(repo => {
+      let imgEl = document.createElement('img');
+      imgEl.setAttribute('src', repo.avatar_url);
+
+      let titleEl = document.createElement('strong');
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement('p');
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement('a');
+      linkEl.setAttribute('target', '_blank');
+      linkEl.setAttribute('href', repo.html_url);
+      linkEl.appendChild(document.createTextNode('Acessar'));
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    })
+  }
+}
+
+new App();
